38fd2b28e599b37bd50f980a14e8c0ca5da87046
Lauren McCoy <lm_lutec@outlook.com>
2020-07-11 21:43:59 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,33 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component{
+
+    state = {
+        reviews: []
+    }
+
+    componentDidMount(){
+        this.getReviews()
+    }
+
+    render(){
+        return(
+            <div className="latest-movie-reviews">
+               {this.displayReviews()}
+            </div>
+        )
+    }
+
+    getReviews = () => {
+        fetch(`${URL}`)
+            .then(res => res.json())
+            .then(json => this.setState({ reviews: json.results}))
+            
+    }
+
+    displayReviews = () => {
+        // console.log(this.state.reviews)
+        this.state.reviews.map((review) => <li key={review.id}>{review}</li>)
+    }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,38 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends React.Component {
+
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+               Movie Review Search:<input type="text" name="search" value={this.state.searchTerm} onChange={this.handleInput} />
+               <input type="Submit" />
+            </form>
+        )
+    }
+
+    grabOneReview = () => {
+        let search = this.state.searchTerm
+
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${search}`)
+            .then(res => res.json())
+            .then(json => console.log(json))
+    }
+
+    handleInput = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        event.preventDefault()
+        // console.log(this.state.searchTerm)
+    }
+}
